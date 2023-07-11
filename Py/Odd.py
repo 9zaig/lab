69e896e494ca27671bc704a24f62a725ff8c0caf
zaig <dday323@hanyang.ac.kr>
2023-07-11 23:25:09 +0900

    Odd column extraction

--- Py/Odd.py
+++ Py/Odd.py
@@ -3,6 +3,7 @@ import pandas as pd
 input='5um.xlsx'
 input_file = "./Input/"+input
 output_file = "./Output/"+input
+##################################
 df = pd.read_excel(input_file)
 first_column = df.iloc[:, 0]
 selected_columns = df.iloc[:, 1::2]
